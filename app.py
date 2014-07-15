897ed99c1876fd8fe114da88e7235a6ecb76af8f
Andrew Benson <adbenson@cmu.edu>
2014-07-15 02:18:16 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+SET_DEBUG = True
+
+@app.route("/")
+def home():
+  return render_template("base.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template("error404.html"), 404
+
+if __name__ == "__main__":
+  app.run(debug=SET_DEBUG)
